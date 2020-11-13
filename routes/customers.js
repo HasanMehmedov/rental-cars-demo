899fe020fdf7782ed0fed9dca6deeecef64fffa6
Hasan Mehmedov <hassi4a@gmail.com>
2020-11-13 17:58:37 +0200

    Handled http get request with id for customers

--- routes/customers.js
+++ routes/customers.js
@@ -13,16 +13,40 @@ router.get('/', async (req, res) => {
     }
 });
 
-async function getCustomers(){
+router.get('/:id', async (req, res) => {
+    const customerId = req.params.id;
+
+    try {
+        const customer = await getCustomer(customerId);
+        res.send(customer);
+    }
+    catch (err) {
+        res.status(err.status).send(err.message);
+    }
+});
+
+async function getCustomers() {
     const customers = await Customer.find();
 
-    if(!customers || customers.length === 0){
+    if (!customers || customers.length === 0) {
         const notFoundError = new Error('There are no saved customers.');
         notFoundError.status = 404;
         throw notFoundError;
     }
 
     return customers;
+}
+
+async function getCustomer(id) {
+    const customer = await Customer.findById(id);
+
+    if(!customer) {
+        const notFoundError = new Error(`Customer with ID: ${id} was not found.`);
+        notFoundError.status = 404;
+        throw notFoundError;
+    }
+
+    return customer;
 }
 
 module.exports = router;
