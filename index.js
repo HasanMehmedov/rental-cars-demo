b9fee37a873dff1ee05e82bb31ee78d4e0b79140
Hasan <hasanmehh@gmail.com>
2020-12-13 11:47:26 +0200

    Logged exceptions and handled uncaught exceptions and unhandled promise rejections

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-require('express-async-errors');
 const express = require('express');
-const mongoose = require('mongoose');
+require('express-async-errors');
 const app = express();
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const config = require('config');
-const cars = require('./routes/cars');
-const customers = require('./routes/customers');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const home = require('./routes/home');
-const error = require('./middlewares/error');
+const winston = require('winston');
 
-if (!config.get('jwtPrivateKey')) {
-    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-    process.exit(1);
-}
-
-app.use(express.json());
-app.use('/api/cars', cars);
-app.use('/api/customers', customers);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/', home);
-app.use(error);
-
-mongoose.connect('mongodb://localhost/cars', { poolSize: 100 })
-    .then(() => console.log('Succesfully connected to MongoDB...'))
-    .catch(() => console.error('Could not connect to the database.'));
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
 
 const port = process.env.port || 3000;
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
